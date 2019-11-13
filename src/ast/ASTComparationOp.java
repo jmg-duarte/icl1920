9bf4ab8bd37ac875200e595620e13837e633d928
Mariana <md.bexiga@campus.fct.unl.pt>
2019-11-13 20:14:22 +0000

    Implement New

---
+++ src/ast/ASTComparationOp.java
@@ -0,0 +1,67 @@
+package ast;
+
+import compiler.Assembler;
+import compiler.CoreCompiler;
+import env.Environment;
+import value.IValue;
+import value.TypeErrorException;
+import value.VBool;
+import value.VInt;
+
+public class ASTComparationOp implements ASTNode {
+    ASTNode exp1;
+    ASTNode exp2;
+    String op;
+
+    public ASTComparationOp(String op, ASTNode exp1, ASTNode exp2){
+        this.exp1 = exp1;
+        this.exp2 = exp2;
+        this.op = op;
+    }
+
+    @Override
+    public IValue eval(Environment env) throws TypeErrorException {
+        VInt v1 = VInt.check(exp1.eval(env));
+        VInt v2 = VInt.check(exp2.eval(env));
+
+            switch (op) {
+                case "==":
+                    if (v1.getValue() == v2.getValue())
+                        return new VBool(true);
+                    else
+                        return new VBool(false);
+                case ">":
+                    if(v1.getValue() > v2.getValue())
+                        return new VBool(true);
+                    else
+                        return new VBool(false);
+                case "<":
+                    if(v1.getValue() < v2.getValue())
+                        return new VBool(true);
+                    else
+                        return new VBool(false);
+                case ">=":
+                    if(v1.getValue() >= v2.getValue())
+                        return new VBool(true);
+                    else
+                        return new VBool(false);
+                case "<=":
+                    if(v1.getValue() <= v2.getValue())
+                        return new VBool(true);
+                    else
+                        return new VBool(false);
+                case "!=":
+                    if(v1.getValue() != v2.getValue())
+                        return new VBool(true);
+                    else
+                        return new VBool(false);
+                default:
+                    throw new IllegalStateException("unexpected operator: " + op);
+            }
+        }
+
+    @Override
+    public Assembler compile(CoreCompiler compiler, Environment env) {
+        return null;
+    }
+}
