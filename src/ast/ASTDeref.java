856488119b2fcee37ad9ceb9b65c455efa88af78
José Duarte <jmg.duarte@campus.fct.unl.pt>
2019-11-13 14:05:34 +0000

    Implement Deref

---
+++ src/ast/ASTDeref.java
@@ -0,0 +1,30 @@
+package ast;
+
+import compiler.Assembler;
+import compiler.CoreCompiler;
+import env.Environment;
+import value.IValue;
+import value.VRef;
+
+public class ASTDeref implements ASTNode {
+
+    private final ASTNode expression;
+
+    public ASTDeref(ASTNode expression) {
+        this.expression = expression;
+    }
+
+    public ASTNode getExpression() {
+        return expression;
+    }
+
+    @Override
+    public IValue eval(Environment env) {
+        return VRef.check(expression.eval(env)).get();
+    }
+
+    @Override
+    public Assembler compile(CoreCompiler compiler, Environment env) {
+        return null;
+    }
+}
