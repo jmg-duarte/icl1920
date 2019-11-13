033bda45de51a5c075cf651ee2dea46e954a6326
José Duarte <jmg.duarte@campus.fct.unl.pt>
2019-11-13 19:12:11 +0000

    Implement While

--- src/ast/ASTWhile.java
+++ src/ast/ASTWhile.java
@@ -1,8 +1,34 @@
 package ast;
 
-public class ASTWhile {
+import compiler.Assembler;
+import compiler.CoreCompiler;
+import env.Environment;
+import value.IValue;
+import value.VBool;
 
-    public ASTWhile(ASTNode exp1, ASTNode exp2, ASTNode exp3){
+public class ASTWhile implements ASTNode {
+    private final ASTNode conditional;
+    private final ASTNode body;
 
+    public ASTWhile(ASTNode conditional, ASTNode body) {
+        this.conditional = conditional;
+        this.body = body;
+    }
+
+    @Override
+    public IValue eval(Environment env) {
+        Environment loopEnv = env.startScope();
+        VBool b = VBool.check(conditional.eval(loopEnv));
+        IValue res = b;
+        while (b.getValue()) {
+            res = body.eval(loopEnv);
+            b = VBool.check(conditional.eval(loopEnv));
+        }
+        return res;
+    }
+
+    @Override
+    public Assembler compile(CoreCompiler compiler, Environment env) {
+        return null;
     }
 }
