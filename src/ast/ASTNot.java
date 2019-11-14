73fcfd0e3c6a423e2b220fdafaafa97ca2ffaf29
Mariana <md.bexiga@campus.fct.unl.pt>
2019-11-14 09:53:09 +0000

    Implement Not

---
+++ src/ast/ASTNot.java
@@ -0,0 +1,26 @@
+package ast;
+
+import compiler.Assembler;
+import compiler.CoreCompiler;
+import env.Environment;
+import value.IValue;
+import value.VBool;
+
+public class ASTNot implements ASTNode {
+    ASTNode exp;
+
+    public ASTNot(ASTNode exp){
+        this.exp = exp;
+    }
+
+    @Override
+    public IValue eval(Environment env) {
+        VBool result = VBool.check(exp.eval(env));
+        return new VBool(!result.getValue());
+    }
+
+    @Override
+    public Assembler compile(CoreCompiler compiler, Environment env) {
+        return null;
+    }
+}
