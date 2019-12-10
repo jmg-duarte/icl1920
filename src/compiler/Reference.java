daa5a32628e40b14bc282ab68dde04456a3634e9
Mariana <md.bexiga@campus.fct.unl.pt>
2019-12-10 22:52:28 +0000

    Add Reference

---
+++ src/compiler/Reference.java
@@ -0,0 +1,38 @@
+package compiler;
+
+import java.io.IOException;
+
+public class Reference {
+
+    private final String refID;
+    private final LineBuilder lb = new LineBuilder();
+
+    public Reference(String refID, String type) {
+        this.refID = refID;
+        lb.appendLine(".class " + refID);
+        lb.appendLine(".super java/lang/Object");
+        lb.appendLine(".field public " + type + ";");
+    }
+
+    public String getReferenceID() {
+        return refID;
+    }
+
+    public void addField(String fieldID) {
+        lb.appendLine(".field public _" + fieldID + " I");
+    }
+
+    public void dumpFrame() throws IOException {
+        lb.appendLine(".method public <init>()V");
+        lb.appendLine("aload_0");
+        lb.appendLine("invokenonvirtual java/lang/Object/<init>()V");
+        lb.appendLine("return");
+        lb.appendLine(".end method");
+        lb.writeToFile(refID + ".j");
+    }
+
+    @Override
+    public String toString() {
+        return refID;
+    }
+}
