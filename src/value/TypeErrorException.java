cc9f44d1f5b8b0ceab5d7b1f55f785643bb88ac8
José Duarte <jmg.duarte@campus.fct.unl.pt>
2019-12-17 17:45:46 +0000

    Add constructor to TypeErrorException

--- src/value/TypeErrorException.java
+++ src/value/TypeErrorException.java
@@ -5,4 +5,8 @@ public class TypeErrorException extends RuntimeException {
     public TypeErrorException() {
         super("illegal arguments");
     }
+
+    public TypeErrorException(String message) {
+        super(message);
+    }
 }
