9389473b65062fce94b7b62e28ad8c35c91bf4e2
José Duarte <jmg.duarte@campus.fct.unl.pt>
2019-12-18 23:09:53 +0000

    Refactor

--- src/compiler/Reference.java
+++ src/compiler/Reference.java
@@ -8,28 +8,25 @@ import types.TRef;
 import java.io.IOException;
 
 
-public class Reference {
+public class Reference implements Dumpable {
 
     private final String refID;
+    private final IType type;
     private final LineBuilder lb = new LineBuilder();
 
     public Reference(IType type) {
+        this.type = type;
         this.refID = TRef.getReferenceClass(type);
-        lb.appendLine(".class public " + TRef.getReferenceClass(type));
-        lb.appendLine(".super java/lang/Object");
-        lb.appendLine(".field public v " + type.getCompiledType());
+        addHeader();
+        addPrologue();
     }
 
     public String getReferenceID() {
         return refID;
     }
 
-    public void dumpReference() throws IOException {
-        lb.appendLine(".method public <init>()V");
-        lb.appendLine("aload_0");
-        lb.appendLine("invokenonvirtual java/lang/Object/<init>()V");
-        lb.appendLine("return");
-        lb.appendLine(".end method");
+    @Override
+    public void dump() throws IOException {
         lb.writeToFile(refID + ".j");
     }
 
@@ -38,5 +35,17 @@ public class Reference {
         return refID;
     }
 
+    private void addHeader() {
+        lb.appendLine(".class public " + TRef.getReferenceClass(type));
+        lb.appendLine(".super java/lang/Object");
+        lb.appendLine(".field public v " + type.getCompiledType());
+    }
 
+    private void addPrologue() {
+        lb.appendLine(".method public <init>()V");
+        lb.appendLine("aload_0");
+        lb.appendLine("invokenonvirtual java/lang/Object/<init>()V");
+        lb.appendLine("return");
+        lb.appendLine(".end method");
+    }
 }
